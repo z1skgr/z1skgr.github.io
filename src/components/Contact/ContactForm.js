7597ee75e979d6b22f054792b9c8ac8aa0b4049e
C.Ziskas <z1skgr@yahoo.gr>
2023-11-16 12:01:26 +0200

    update ContactForm.js
    Validation methods for errors im mail sent

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -9,11 +9,15 @@ import logo from "../../Assets/logo2.gif";
 import Swal from 'sweetalert2';
 import emailjs from 'emailjs-com';
 
+
+
 const SERVICE_ID = "service_ypirwh1"
 const TEMPLATE_ID = "template_024cef7"
 const USER_ID = "xPyqSIU6BgWGIiiKr"
 
 function ContactForm() {
+
+  const emailRef = useRef(null);
 
   const [formIn, setForm] = useState({
     from_name: '',
@@ -21,12 +25,16 @@ function ContactForm() {
     message: '',
   })
 
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
 
   const handleName = (event) => {
     setForm({
       ...formIn,
       from_name: event.target.value
     })
+    
 
   }
 
@@ -35,6 +43,7 @@ function ContactForm() {
       ...formIn,
       from_email: event.target.value
     })
+    
 
   }
 
@@ -52,31 +61,57 @@ function ContactForm() {
   function sendEmail(e) {
     e.preventDefault();
     e.stopPropagation();
-    if(!formIn.from_email || !formIn.from_name || !formIn.message){
-      alert("Please fill in all fields");
-    }
-    else{
-      
-      emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-      .then((result) => {
-          console.log(result.text);
-          Swal.fire({
-            icon: 'success',
-            title: 'Message Sent Successfully'
-          })
-      }, (error) => {
-          console.log(error.text);
-          Swal.fire({
+    if(!formIn.from_email || !formIn.from_name || !formIn.message)
+    {
+      Swal.fire({
+        icon: 'error',
+        title: 'Ooops, something went wrong',
+        text: "Please fill in all fields"})
+    }else if
+    (!isValidEmail(emailRef.current.value)) {
+      console.log(`exoume ${emailRef.current.value}`)
+      Swal.fire({
             icon: 'error',
             title: 'Ooops, something went wrong',
-            text: error.text,
+            text: "Insert valid email"
         });
+    }
+    else{
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, sent it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+          .then((result) => {
+              console.log(result.text);
+              Swal.fire({
+                text: "Your message has been sent.",
+                icon: 'success',
+                title: 'Message Sent Successfully',
+              })
+          }, (error) => {
+              console.log(error.text);
+              Swal.fire({
+                icon: 'error',
+                title: 'Ooops, something went wrong',
+                text: error.text,
+            });
+          });
+          setForm({
+            from_name: '',
+            from_email: '',
+            message: '',
+          })
+        }
+        
       });
-      setForm({
-        from_name: '',
-        from_email: '',
-        message: '',
-      })
+
     }
     
   }
@@ -98,7 +133,7 @@ function ContactForm() {
           <form ref={form} onSubmit={sendEmail}>
   <div className="form-group" >
     <label htmlFor="from_email">Email address</label>
-    <input id="from_email"  value={formIn.from_email}  type="email" name="from_email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" onChange={handleEmail}/>
+    <input ref={emailRef} id="from_email"  value={formIn.from_email} name="from_email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" onChange={handleEmail}/>
     <small id="emailHelp"  className="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div className="form-group">
